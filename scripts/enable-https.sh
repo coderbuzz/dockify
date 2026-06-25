4be74659095e58d7722ea739bc16090a4035d3e3
coderbuzz <indra.sync@gmail.com>
2026-06-25 01:41:20 +0000

    chore: remove obsolete files

--- scripts/enable-https.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "=== Enabling HTTPS on Caddy ==="
-docker exec caddy curl -s -X PUT http://localhost:2019/config/apps/http/servers/srv0/listen \
-  -H 'Content-Type: application/json' \
-  -d '[":80",":443"]'
-echo ""
-echo "Done. HTTPS enabled on port 443."
